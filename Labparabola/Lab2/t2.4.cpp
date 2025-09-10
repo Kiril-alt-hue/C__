56514ff3deff15e51476718f7e8026f3f5bddca5
kiril <kirabatrak@gmail.com>
2025-09-10 23:25:19 +0300

    homework 2 first version

--- Labparabola/Lab2/t2.4.cpp
+++ Labparabola/Lab2/t2.4.cpp
@@ -1,15 +1,25 @@
 #include <stdio.h>
-#include <math.h>
 
-double f2(double x) {
-    double y = (pow(x, 5) - 1)/(x - 1);
-    return y;
+double calculate_polynomial(double x) {
+
+    double result = 1.0;
+    result = result * x + 5.0;
+    result = result * x + 10.0;
+    result = result * x + 10.0;
+    result = result * x + 5.0;
+    result = result * x + 1.0;
+
+    return result;
 }
 
 int main() {
     double x;
+
     printf("Enter x: ");
     scanf("%lf", &x);
 
-    printf("y = %.2lf\n", f2(x));
+    double y = calculate_polynomial(x);
+    printf("y = x^5 + 5x^4 + 10x^3 + 10x^2 + 5x + 1 for x = %.2f: %.2f\n", x, y);
+
+    return 0;
 }
