467ad2aa3b7b43d88c0264c8f5d6001d36f35de2
kiril <kirabatrak@gmail.com>
2025-09-19 23:37:29 +0300

    upgrade t3.12

--- HW/3/t3.12.cpp
+++ HW/3/t3.12.cpp
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     double a, b, c, max, x, y;
+    const double EPS = 1e-9;
 
     printf("Enter sides a, b, c: ");
     scanf("%lf %lf %lf", &a, &b, &c);
 
+    if (a <= 0 || b <= 0 || c <= 0) {
+        printf("Sides must be positive.\n");
+        return 0;
+    }
 
     if (a + b <= c || a + c <= b || b + c <= a) {
         printf("Triangle does not exist.\n");
         return 0;
     }
-
 
     if (a >= b && a >= c) {
         max = a; x = b; y = c;
@@ -21,10 +26,12 @@ int main() {
         max = c; x = a; y = b;
     }
 
+    double left = max * max;
+    double right = x * x + y * y;
 
-    if (max * max == x * x + y * y) {
+    if (fabs(left - right) < EPS) {
         printf("Right triangle.\n");
-    } else if (max * max < x * x + y * y) {
+    } else if (left < right) {
         printf("Acute triangle.\n");
     } else {
         printf("Obtuse triangle.\n");
--- t4.10.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    double a = 1.0;
-    do{a/=2;} while (1!=a+1);
-    printf("a = %e\n", a);
-    return 0;
-}
