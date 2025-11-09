ba8733e01450b136e786984fa9ca9f7209cd450e
kiril <kirabatrak@gmail.com>
2025-11-09 23:33:49 +0200

    clean

--- arrayutils11_1.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARRAYUTILS11_1_H
-#define ARRAYUTILS11_1_H
-
-void writeArrayToFile(const char* filename, const double* arr, int size);
-
-void readAndPrintArrayFromFile(const char* filename);
-
-
-#endif //ARRAYUTILS11_1_H
